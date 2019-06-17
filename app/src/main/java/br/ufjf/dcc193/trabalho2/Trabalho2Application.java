140719e870de12ff39145e359c245bf2e685b324
Ana Carolina Fidelis Gonçalves <acarolinafg@gmail.com>
2019-06-17 19:18:50 -0300

    Início do projeto...

---
+++ app/src/main/java/br/ufjf/dcc193/trabalho2/Trabalho2Application.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trabalho2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho2Application.class, args);
+	}
+
+}
